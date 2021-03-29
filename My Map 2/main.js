ef39d6274e22c502911c60d50bb79bcc3e1dd4a4
Israa <imoqbel@kiitos-tech.com>
2021-03-30 00:07:07 +0300

    Add dome notes to map 2

--- My Map 2/main.js
+++ My Map 2/main.js
@@ -3,10 +3,12 @@
 const tooltips = document.querySelectorAll('.all-tooltips .tooltip');
 const contents = document.querySelector('.tooltip-content .content');
 
-
+// you don't get content elements when you loop through tooltips, name is not semantic here
 tooltips.forEach(contents =>{
     const pin = tooltip.querySelector('.pin');
+    // I believe variables can't have hyphen you can say tooltipContent instead
     const content = tooltip-content.querySelector('.tooltip-content .content');
+    // you need to remove active from other contents before adding it again
     pin.addEventListener('click', ()=>{
         content.classList.add('active');
     })
